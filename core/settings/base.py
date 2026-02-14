a5c234173648f85a5a425aa6f69d2003b627a74d
ExpertGG10 <gustavo.fc.cfc@gmail.com>
2026-02-13 21:47:51 -0300

    Troca para postgresql

--- core/settings/base.py
+++ core/settings/base.py
@@ -81,11 +81,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'enviador.wsgi.application'
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
